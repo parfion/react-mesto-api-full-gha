b3afe953d249d5fb4656d0abc38effe522e81360
Alexander Parfionov <parfion.alexander@yandex.ru>
2023-11-13 14:30:11 +0300

    fix base_url

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -118,7 +118,7 @@ class Api {
 }
 
 const api = new Api({
-	baseUrl: 'https://mesto.parfion.nomoredomainsrocks.ru',
+	baseUrl: 'https://mesto.parfion.nomoredomainsrocks.ru/api',
 	headers: {
 	  'Content-Type': 'application/json'
 	}
